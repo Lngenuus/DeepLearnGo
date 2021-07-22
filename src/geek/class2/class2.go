47808daba66e8830c21ad86313c410419f6c5264
Lngenuus <18575570936@163.com>
2021-07-22 14:22:20 +0800

    update: 更新描述信息

--- src/geek/class2/class2.go
+++ src/geek/class2/class2.go
@@ -32,7 +32,7 @@ func delSqlite(db *sql.DB) {
 	os.Remove("./test.db")
 }
 
-// 通过id查找name
+// 通过id查找name 模拟dao层操作
 func queryName1ById(db *sql.DB, id int) (string, error) {
 	var querySQL = "SELECT name1 from t_test where id = ?"
 	var retName1 string
@@ -40,24 +40,25 @@ func queryName1ById(db *sql.DB, id int) (string, error) {
 	err := db.QueryRow(querySQL, id).Scan(&retName1)
 	if err != nil {
 		// 自定义错误内容
-		return "", xerrors.Wrapf(err, fmt.Sprintf("[queryName1ById] 信息查询失败, id=%v\n", id))
+		return "", xerrors.Wrapf(err, fmt.Sprintf("\n[queryName1ById] 信息查询失败, id=%v\n", id))
 	}
 	return retName1, nil
 }
 
-// 执行函数
+// 执行函数模拟业务层
 func Run() {
 	db := initSqlite()
 	defer delSqlite(db)
 
 	name1, err := queryName1ById(db, 1)
+	// 业务默认需要打印全部错误信息
 	if err != nil {
 		// 错误详细信息输出
 		fmt.Printf("错误信息为:\n%+v\n", err)
 
-		// 根据业务需要执行对应的错误查看
+		// 根据业务需要执行对应的错误处理
 		if errors.Is(err, sql.ErrNoRows) {
-			fmt.Println("\n\n业务侧对应的处理->没有查询到数据")
+			fmt.Println("\n\n业务侧对应的处理->没有查询到数据\n\n")
 		}
 		return
 	}
