06c04672b1dccdac5a0e524d2f063185be85669f
Lngenuus <18575570936@163.com>
2021-08-13 18:39:54 +0800

    update: modify rolling

--- src/geek/class5/rolling/rolling.go
+++ src/geek/class5/rolling/rolling.go
@@ -12,11 +12,9 @@ var defaultConfig = Config{
 }
 
 type Config struct {
-	WindowSec   time.Duration
+	WindowSec   int64
 	SmapleTimes int
 }
-
-type Opts func(c *Config)
 
 // Window 滑动窗口的实现
 type Window struct {
@@ -34,11 +32,11 @@ type Bucket struct {
 }
 
 func unixMilli() int64 {
-	return time.Now().UnixNano() / 1000
+	return time.Now().UnixNano() / 1000000
 }
 
 func (w *Window) skip() int64 {
-	return (time.Now().UnixNano()/1000 - w.lastAddTime) / w.BucketMilli
+	return (time.Now().UnixNano()/1000000 - w.lastAddTime) / w.BucketMilli
 }
 
 func (w *Window) curBucket() *Bucket {
@@ -46,7 +44,7 @@ func (w *Window) curBucket() *Bucket {
 	var ok bool
 	skip := w.skip()
 	if skip > 0 {
-		w.lastAddTime = skip * w.BucketMilli
+		w.lastAddTime = w.lastAddTime + skip*w.BucketMilli
 	}
 	if bucket, ok = w.Buckets[w.lastAddTime]; !ok {
 		bucket = &Bucket{}
@@ -67,20 +65,45 @@ func (w *Window) delBucket() {
 
 func (w *Window) Add(val float64) {
 	w.mu.Lock()
-	defer w.mu.RLock()
-	w.delBucket()
-
+	defer w.mu.Unlock()
 	b := w.curBucket()
 	b.val += val
 	b.count++
 	w.delBucket()
 }
 
-func NewWindow(opts ...Opts) *Window {
-	cfg := defaultConfig
-	for _, o := range opts {
-		o(&cfg)
+func (w *Window) Reduce(f func(map[int64]*Bucket) float64) float64 {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	return f(w.Buckets)
+}
+
+func NewWindow(cfg Config) *Window {
+
+	windowMilli := int64(cfg.WindowSec * 1000)
+	bucketMilli := windowMilli / int64(cfg.SmapleTimes)
+	return &Window{
+		Size:        cfg.SmapleTimes,
+		WindowMilli: windowMilli,
+		Buckets:     make(map[int64]*Bucket),
+		BucketMilli: bucketMilli,
+		lastAddTime: unixMilli(),
+		mu:          &sync.RWMutex{},
 	}
-	fmt.Println(cfg)
-	return &Window{}
+}
+
+func NewDeafultWindow() *Window {
+	cfg := defaultConfig
+	windowMilli := int64(cfg.WindowSec * 1000)
+	bucketMilli := windowMilli / int64(cfg.SmapleTimes)
+	ret := &Window{
+		Size:        cfg.SmapleTimes,
+		WindowMilli: windowMilli,
+		Buckets:     make(map[int64]*Bucket),
+		BucketMilli: bucketMilli,
+		lastAddTime: unixMilli(),
+		mu:          &sync.RWMutex{},
+	}
+	fmt.Println(ret.Size, ret.WindowMilli, ret.Buckets, ret.BucketMilli, ret.lastAddTime)
+	return ret
 }
--- src/geek/class5/rolling/rolling_test.go
+++ src/geek/class5/rolling/rolling_test.go
@@ -1,8 +1,54 @@
 package rolling
 
 import (
+	"fmt"
+	"math"
+	"math/rand"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestNewWindow(t *testing.T) {
+	NewDeafultWindow()
+}
+
+func TestBuckets(t *testing.T) {
+	w := NewDeafultWindow()
+	for key, val := range w.Buckets {
+		fmt.Println("data:", key, val)
+	}
+}
+
+func TestCurBucket(t *testing.T) {
+	w := NewDeafultWindow()
+	b := w.curBucket()
+	fmt.Printf("%v\n", b)
+}
+
+func TestReduce(t *testing.T) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	w := NewDeafultWindow()
+	var wg sync.WaitGroup
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		time.Sleep(time.Duration(r.Intn(100)) * time.Millisecond)
+		go func() {
+			defer wg.Done()
+			time.Sleep(time.Duration(r.Intn(100)) * time.Millisecond)
+			w.Add(1)
+		}()
+	}
+	wg.Wait()
+	ret := w.Reduce(func(m map[int64]*Bucket) float64 {
+		max := 0.0
+		n := 0.0
+		for _, bucket := range m {
+			n++
+			max = math.Max(max, bucket.val)
+			fmt.Println(bucket)
+		}
+		return max
+	})
+	fmt.Println("ret:", ret)
 }
